9785aa28b886a2f1906399c6fb1eef96b8ebb4bc
antonBogomil <anton.bogomil777@gmail.com>
2024-10-31 13:23:01 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,75 +4,76 @@ import { Button, Chip, Paper, Stack, TextField } from '@mui/material';
 import React, { FormEvent, useState } from 'react';
 import { Delete } from '@mui/icons-material';
 import { Circle } from '@/components/Circle';
+import { MAX_LENGTH } from '@/constants';
 
 
 const mockData = [
-    'John Doe',
-    'Alice',
-    'Bob',
-    'Charlie',
-    'David',
+  'John Doe',
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
 ];
 
 export default function Home() {
-    const [input, setInput] = useState('');
-    const [list, setList] = useState<string[]>(mockData);
+  const [input, setInput] = useState('');
+  const [list, setList] = useState<string[]>(mockData);
 
 
-    const handleRemove = (index: number) => () => {
-        setList(list.filter((_, i) => i !== index));
+  const handleRemove = (index: number) => () => {
+    setList(list.filter((_, i) => i !== index));
+  };
+
+  const handleAdd = (e: FormEvent) => {
+    e.preventDefault();
+    if (input && !list.includes(input) && list.length <= MAX_LENGTH) {
+      setList([...list, input]);
+      setInput('');
+    } else {
+      alert('Invalid input');
     }
+  };
 
-    const handleAdd = (e: FormEvent) => {
-        e.preventDefault();
-        if (input && !list.includes(input)) {
-            setList([...list, input]);
-            setInput('');
-        } else {
-            alert('Name is empty or already exists');
-        }
-    }
+  return (
+    <div className="text-center px-10">
+      <form
+        onSubmit={handleAdd}
+        className="flex gap-4 my-2"
+      >
+        <TextField
+          className="flex-grow"
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+          variant="standard"
+          label="Name"
+        />
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          className="w-1/3"
+          type="submit"
+        >
+          Add
+        </Button>
+      </form>
+      {list.length > 0 && (
+        <Paper className="my-4 p-2">
+          <Stack direction="row" spacing={1}>
+            {list.map((name, i) => (
+              <Chip
+                key={name}
+                label={name}
+                onDelete={handleRemove(i)}
+                deleteIcon={<Delete />}
+              >
+              </Chip>
+            ))}
+          </Stack>
+        </Paper>
+      )}
 
-    return (
-        <div className="text-center px-10">
-            <form
-                onSubmit={handleAdd}
-                className="flex gap-4 my-2"
-            >
-                <TextField
-                    className="flex-grow"
-                    value={input}
-                    onChange={(e) => setInput(e.currentTarget.value)}
-                    variant="standard"
-                    label="Name"
-                />
-                <Button
-                    variant="contained"
-                    size="large"
-                    color="primary"
-                    className="w-1/3"
-                    type="submit"
-                >
-                    Add
-                </Button>
-            </form>
-            {list.length > 0 && (
-                <Paper className="my-4 p-2">
-                    <Stack direction="row" spacing={1}>
-                        {list.map((name, i) => (
-                            <Chip
-                                key={name}
-                                label={name}
-                                onDelete={handleRemove(i)}
-                                deleteIcon={<Delete />}
-                            >
-                            </Chip>
-                        ))}
-                    </Stack>
-                </Paper>
-            )}
-
-            {list.length > 1 && <Circle data={list} />}
-        </div>
-    );
+      {list.length > 1 && <Circle data={list} />}
+    </div>
+  );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const MAX_LENGTH = 20;
+export const MAX_LENGTH = 8;
 export const COLORS = [
-    '#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#33FFF5', '#F5FF33', '#FF8C33', '#8C33FF', '#33FF8C', '#FF3333',
-    '#33FF33', '#3333FF', '#FF33FF', '#33FFFF', '#FFFF33', '#FF6633', '#6633FF', '#33FF66', '#FF3366', '#3366FF',
+  '#FAA057', '#4D80D1', '#c23e8f', '#92D565', '#AC6FC7', '#EB4463', '#320d6d', '#c5efcb',
 ];
